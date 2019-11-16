ed452447a5b5c91cc455211edfcdb8e62060f33a
JoseRobertoECJr <joserobertoecjr@gmail.com>
2019-11-16 19:39:03 -0300

    +sparql query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DbpediaSparqlService } from './dbpedia-sparql.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private dbpediaSparqlService: DbpediaSparqlService) {
+
+  }
   title = 'dbpedia-sparql-interface';
+
+  ngOnInit() {
+    this.dbpediaSparqlService.getSparQL(
+      `select distinct ?prop ?value where { 
+        ?class a owl:Class;
+               rdfs:label "movie"@en;
+               ?prop ?value.
+       }`
+    ).subscribe((data) => console.log(data))
+
+  }
 }
---
+++ src/app/dbpedia-sparql.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { UrlParamEncodingService } from './url-param-encoding.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbpediaSparqlService {
+
+  constructor(private http: HttpClient, private urlParamEncodingService: UrlParamEncodingService) { }
+
+  /**
+   * get SparQL DBPedia Results
+   * */
+  public getSparQL(query: string) {
+    return this.http.get(
+      'https://dbpedia.org/sparql',
+      {
+        params: new HttpParams({
+          fromObject: {
+          "default-graph-uri": "http://dbpedia.org",
+          "query": query,
+          "format": "application/sparql-results+json",
+          "CXML_redir_for_subjs": "121",
+          "CXML_redir_for_hrefs": "",
+          "timeout": "30000",
+          "debug": "on",
+          "run": "Run Query",
+          },
+          encoder: this.urlParamEncodingService
+      })
+    })
+  }
+
+}
