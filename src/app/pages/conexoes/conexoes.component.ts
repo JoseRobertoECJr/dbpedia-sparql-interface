5b4b072c0eb1a56aa81afbd8c75b344e0071e1a0
JoseRobertoECJr <joserobertoecjr@gmail.com>
2019-11-17 23:16:59 -0300

    #melhorias

--- src/app/pages/conexoes/conexoes.component.ts
+++ src/app/pages/conexoes/conexoes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DbpediaSparqlService } from 'src/app/dbpedia-sparql.service';
-import * as vis from "vis";
+import { Network } from "vis";
+import { graphOptions } from "src/app/pages/conexoes/graph-options"
 
 @Component({
   selector: 'app-conexoes',
@@ -9,11 +10,38 @@ import * as vis from "vis";
 })
 export class ConexoesComponent implements OnInit {
 
-  sparqlQuery: string
+  network: Network
+
+  type: string
+  label: string
+  prop: string
+  res: any
+
   result: string
   nodes: Array<any>
   edges: Array<any>
   lastIndex: number
+
+  buildQuery(){
+    let query =
+      `
+        SELECT DISTINCT ?obj ?prop (SAMPLE(?value) AS ?value) WHERE {
+                        ?obj a dbo:${this.type};
+                        rdfs:label ?name;
+                        ?prop ?value.
+                        
+                        FILTER (lang(?name) = 'en')
+      `;
+
+      if(this.label)
+        query += ` filter(str(?name) = "${this.label}") `;
+
+      if(this.prop)
+        query += ` filter( regex( str(?prop), "${this.prop}") ) `;
+
+      query += ` } GROUP BY ?obj ?prop`
+      return query;
+  }
 
 
   constructor(private dbpediaSparqlService: DbpediaSparqlService) {
@@ -21,8 +49,6 @@ export class ConexoesComponent implements OnInit {
   }
 
   initGraph(nodes, edges){
-    console.log(nodes)
-    console.log(edges)
     var container = document.getElementById("mynetwork");
 
     var data = {
@@ -30,172 +56,71 @@ export class ConexoesComponent implements OnInit {
       edges: edges
     };
 
-    var options = {
-      nodes: {
-      shape: "dot",
-      size: 20,
-      font: {
-        size: 15,
-        color: "gray"
-      },
-      borderWidth: 2
-    },
-    edges: {
-      width: 2
-    }};
+    const options = graphOptions;
 
-    var network = new vis.Network(container, data, options);
+    this.network = new Network(container, data, options);
+
+    this.network.on( 'click', (properties) => {
+
+      const index = properties.nodes.pop();
+      console.log(this.res[index - 1])
+    });
+  }
+
+  focus(){
+    this.network.focus(0)
+  }
+
+  zoomIn(){
+    const ficOp = {
+      nodes: ["0"],
+      animation: false
+    }
+    this.network.fit(ficOp)
+  }
+
+  zoomOut(){
+    this.network.fit()
   }
 
   ngOnInit() {
-    //this.thatEx()
   }
 
-  // SELECT DISTINCT ?obj ?prop ?value WHERE {
-  //   ?obj a dbo:Film;
-  //          rdfs:label ?name;
-  //          ?prop ?value.
-  //   filter(regex(str(?name), "La La Land"))
-  // }
-   
-
   search(id: number){
-    console.log(id)
-    this.dbpediaSparqlService.getSparQL(this.sparqlQuery).subscribe((data) => {
+    this.dbpediaSparqlService.getSparQL(this.buildQuery()).subscribe((data) => {
       const result = JSON.stringify(data)
-      console.log(data)
-        const nodes = data.results.bindings.map((node, index) => {
+      this.res = data.results.bindings.filter( (obj) => obj.value["xml:lang"] == "en" || obj.value["xml:lang"] == undefined)
+      
+      const labelNode = this.res.find((obj) => obj.prop.value.split("/").pop().split("#").pop() == "label")
+      this.res.splice(this.res.indexOf(labelNode), 1)
+
+
+      const nodes = this.res.map((node, index) => {
           const indexx = index + this.lastIndex + 1
+          const group = node.value.type.split("/").pop()
           return {
             id: indexx,
-            label: node.value.type + "(" + node.value.value.split("/").pop().substring(0, 15) + ")",
-            group: node.value.type.split("/").pop() }
+            label: node.value.value.split("/").pop().substring(0, 20),
+            group: group == "typed-literal" ? "literal" : group }
         })
 
-        console.log(this.nodes)
 
-        const edges = data.results.bindings.map((node, index) => {
+        const edges = this.res.map((node, index) => {
           const indexx = index + this.lastIndex + 1
 
           return {
             from: id,
-            to: indexx
+            to: indexx,
+            label: node.prop.value.split("/").pop().split("#").pop()
           }
         })
 
         if(id == 0)
-          nodes.push({ id: 0, label: "MAIN NODE", group: "MAIN NODE" })
+          nodes.push({ id: 0, label: labelNode.value.value.split("/").pop().substring(0, 15), group: "MAIN NODE" })
 
         this.initGraph(nodes, edges)
     })
 
-  }
-
-
-  thatEx(){
-    
-    var nodes = [
-      { id: 0, label: "0", group: "source" },
-      { id: 1, label: "1", group: "icons" },
-      { id: 2, label: "2", group: "icons" },
-      { id: 3, label: "3", group: "icons" },
-      { id: 4, label: "4", group: "icons" },
-      { id: 5, label: "5", group: "icons" },
-      { id: 6, label: "6", group: "icons" },
-      { id: 7, label: "7", group: "icons" },
-      { id: 8, label: "8", group: "icons" },
-      { id: 9, label: "9", group: "icons" },
-      { id: 10, label: "10", group: "mints" },
-      { id: 11, label: "11", group: "mints" },
-      { id: 12, label: "12", group: "mints" },
-      { id: 13, label: "13", group: "mints" },
-      { id: 14, label: "14", group: "mints" },
-      { id: 15, group: "dotsWithLabel" },
-      { id: 16, group: "dotsWithLabel" },
-      { id: 17, group: "dotsWithLabel" },
-      { id: 18, group: "dotsWithLabel" },
-      { id: 19, group: "dotsWithLabel" },
-      { id: 20, label: "diamonds", group: "diamonds" },
-      { id: 21, label: "diamonds", group: "diamonds" },
-      { id: 22, label: "diamonds", group: "diamonds" },
-      { id: 23, label: "diamonds", group: "diamonds" }
-    ];
-    var edges = [
-      { from: 1, to: 0, label: "label1", font: { size: 20, background: "white" } },
-      { from: 2, to: 0 },
-      { from: 4, to: 3 },
-      { from: 5, to: 4 },
-      { from: 4, to: 0 },
-      { from: 7, to: 6 },
-      { from: 8, to: 7 },
-      { from: 7, to: 0 },
-      { from: 10, to: 9 },
-      { from: 11, to: 10 },
-      { from: 10, to: 4 },
-      { from: 13, to: 12 },
-      { from: 14, to: 13 },
-      { from: 13, to: 0 },
-      { from: 16, to: 15 },
-      { from: 17, to: 15 },
-      { from: 15, to: 10 },
-      { from: 19, to: 18 },
-      { from: 20, to: 19 },
-      { from: 19, to: 4 },
-      { from: 22, to: 21 },
-      { from: 23, to: 22 },
-      { from: 23, to: 0 }
-    ];
-    
-    // create a network
-    var container = document.getElementById("mynetwork");
-    var data = {
-      nodes: nodes,
-      edges: edges
-    };
-    var options = {
-      nodes: {
-        shape: "dot",
-        size: 20,
-        font: {
-          size: 15,
-          color: "gray"
-        },
-        borderWidth: 2
-      },
-      edges: {
-        width: 2
-      },
-      groups: {
-        diamonds: {
-          shape: "icon",
-          icon: {
-            face: "FontAwesome",
-            code: "\uf04d",
-            size: 50,
-            color: { background: "red", border: "white" }
-          }
-        },
-        dotsWithLabel: {
-          label: "I'm a dot!",
-          shape: "dot",
-          color: "cyan"
-        },
-        mints: { color: "rgb(0,255,140)" },
-        icons: {
-          shape: "icon",
-          icon: {
-            face: "FontAwesome",
-            code: "\uf0c0",
-            size: 50,
-            color: "orange"
-          }
-        },
-        source: {
-          color: { border: "gray" }
-        }
-      }
-    };
-    var network = new vis.Network(container, data, options);
   }
 
 }
---
+++ src/app/pages/conexoes/graph-options.ts
@@ -0,0 +1,35 @@
+export const graphOptions = {
+    nodes: {
+        shape: "dot",
+        size: 20,
+        font: {
+        size: 15,
+        color: "gray"
+        },
+        borderWidth: 2
+    },
+    edges: {
+        width: 2
+    },
+    groups: {
+        literal: {
+            shape: "icon",
+            icon: {
+                face: "FontAwesome",
+                code: "\uf0c8",
+                size: 50,
+                color: "#cdd160"
+            }
+        }
+    },
+    physics: {
+        hierarchicalRepulsion: {
+            centralGravity: 0.0,
+            springLength: 100,
+            springConstant: 0.001,
+            nodeDistance: 100,
+            damping: 0.09
+        },
+        solver: 'hierarchicalRepulsion'
+    }
+}
